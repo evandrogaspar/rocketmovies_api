34d10af381cf303fec5990b8584ff7dae1ae365e
*Evandro Gaspar <Fgaspar15@hotmail.com>
2023-05-14 14:45:27 +0100

    Implementação da função index para exibir todos os registros da tabela movie_notes e outro index para a tabela movie_tags

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -7,7 +7,11 @@ const movieRoutes = Router()
 const movieController = new MovieController()
 
 movieRoutes.post("/:user_id", movieController.create)
+
+movieRoutes.get("/", movieController.index)
+
 movieRoutes.get("/:id", movieController.show)
+
 movieRoutes.delete("/:id", movieController.delete)
 
 
